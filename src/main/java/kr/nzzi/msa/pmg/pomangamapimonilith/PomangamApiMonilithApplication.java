95a63a213211ec66f1f894eab35d4cf38a4c6935
Nzzi <cholnh1@naver.com>
2021-07-28 14:09:23 +0900

    Initial commit

---
+++ src/main/java/kr/nzzi/msa/pmg/pomangamapimonilith/PomangamApiMonilithApplication.java
@@ -0,0 +1,13 @@
+package kr.nzzi.msa.pmg.pomangamapimonilith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomangamApiMonilithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomangamApiMonilithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/nzzi/msa/pmg/pomangamapimonilith/PomangamApiMonilithApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.nzzi.msa.pmg.pomangamapimonilith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomangamApiMonilithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
