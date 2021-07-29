ea5989dff5f1e8bebbb65cb17d003a605666af69
Nzzi <cholnh1@naver.com>
2021-07-29 14:51:30 +0900

    edit cicd 오류추정지점 수정

--- src/main/java/kr/nzzi/msa/pmg/pomangamapimonilith/domain/rabbitmq/handler/RabbitMqExceptionHandler.java
+++ src/main/java/kr/nzzi/msa/pmg/pomangamapimonilith/domain/rabbitmq/handler/RabbitMqExceptionHandler.java
@@ -14,10 +14,10 @@ public class RabbitMqExceptionHandler extends RejectAndDontRequeueRecoverer {
 
         final byte[] body = message.getBody();
         final String msg = new String(body, StandardCharsets.UTF_8);
-        log.error("===================");
+        log.error("====================");
         log.debug(msg);
         log.warn("Retries exhausted for message " + message, cause);
-        log.error("===================");
+        log.error("====================");
     }
 
 }
